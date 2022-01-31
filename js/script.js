004309f847efa64d035b8077ad4ba29572bd37e0
Feruza <feryzaxaydarova2@gmail.com>
2022-01-31 12:14:40 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const loader = document.querySelector(".loader");
+  setTimeout(function () {
+    loader.style.opacity = "0";
+    setTimeout(function () {
+      loader.style.display = "none";
+    }, 1000);
+  }, 2000);
+});
